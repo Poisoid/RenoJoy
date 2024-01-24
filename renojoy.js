7fde6b1c8d2caac0efb6d446262cc8f513162674
Poisoid <terrenceajy@yahoo.com>
2024-01-24 12:23:31 +0800

    Co-authored-by: Yur1ne <Yur1ne@users.noreply.github.com>

--- renojoy.js
+++
@@ -1,9 +0,0 @@
-function showMenu() {
-  const menu = document.getElementById("menu");
-  menu.style.transform = "translateX(0)";
-}
-
-function hideMenu() {
-  const menu = document.getElementById("menu");
-  menu.style.transform = "translateX(150px)";
-}
