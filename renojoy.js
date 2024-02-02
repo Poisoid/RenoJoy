3ce194fb0cd6506c0b70500b1f0ef695e653fd90
Poisoid <terrenceajy@yahoo.com>
2024-02-02 12:21:31 +0800

    new login page

--- renojoy.js
+++ renojoy.js
@@ -1,9 +1,25 @@
-function showMenu() {
-  const menu = document.getElementById("menu");
-  menu.style.transform = "translateX(0)";
+// Define the function to perform the AJAX request
+function performAjaxRequest() {
+  var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://restdb.io/account/databases/integratedproject-9aca/api",
+      "method": "GET",
+      "headers": {
+          "content-type": "application/json",
+          "x-apikey": "65b8a6643d3b7f7e97c26564",
+          "cache-control": "no-cache"
+      }
+  };
+
+  $.ajax(settings).done(function (response) {
+      console.log(response);
+  });
 }
 
-function hideMenu() {
-  const menu = document.getElementById("menu");
-  menu.style.transform = "translateX(150px)";
-}
+// Attach the function to the click event of the login button
+$(document).ready(function () {
+  $("#container-login100-form-btn").click(function () {
+      performAjaxRequest();
+  });
+});
